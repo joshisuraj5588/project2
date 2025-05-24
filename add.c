a138b35136f601217834e7f6396d93b88b0d854e
Pradeep Pant <pantpradeep621@gmail.com>
2025-05-24 17:04:43 +0545

    Added substraction on add.c file

--- add.c
+++ add.c
@@ -7,9 +7,10 @@ int main() {
     int num1, num2, sum;
     printf("Enter two integers: ");
     scanf("%d %d", &num1, &num2);
-    
     sum = add(num1, num2);
-    
     printf("Sum: %d\n", sum);
+    //subtraction
+    int difference = num1 - num2;
+    printf("Difference: %d\n", difference);
     return 0;
 }
